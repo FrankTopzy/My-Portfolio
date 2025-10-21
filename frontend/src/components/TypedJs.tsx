0af9c70c06078dd7df09cfac0e461e2c65873ebc
FrankTopzy <166184827+FrankTopzy@users.noreply.github.com>
2025-10-21 08:56:18 +0100

    Created the typedJs component

---
+++ frontend/src/components/TypedJs.tsx
@@ -0,0 +1,20 @@
+import { ReactTyped } from "react-typed";
+
+const TypedJs = () => (
+  <ReactTyped
+    strings={[
+      "FRONTEND DEVELOPER.",
+      "FOREX TRADER.",
+      "GRAPHICS DESIGNER.",
+    ]}
+    typeSpeed={40}
+    backSpeed={50}
+    attr="placeholder"
+    loop
+    className="uppercase"
+  >
+    <input type="text" />
+  </ReactTyped>
+);
+
+export default TypedJs
