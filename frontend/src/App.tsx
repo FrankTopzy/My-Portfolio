b8563bb636ad3c794105da2024178e345440c3d9
Frank <temitopeadeoye787898@gmail.com>
2025-10-24 08:43:21 +0100

    Added the state to handle responsive navbar.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,19 @@
+import { useState } from "react"
+import FollowCursor from "./components/Cursor"
 import Footer from "./components/Footer"
 import Navbar from "./components/Navbar/Navbar"
 import Home from "./Homepage/Home"
 
 
 function App() {
-
+  const [isOpen, setIsOpen] = useState<boolean>(false)
 
   return (
     <>
-      <Navbar/>
+      <Navbar setIsOpen={setIsOpen} isOpen={isOpen}/>
       <Home/>
       <Footer/>
+      <FollowCursor/>
     </>
   )
 }
