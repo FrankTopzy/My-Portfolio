58c959009b588a7c4615c1ec97fefda263415d6b
Frank <temitopeadeoye787898@gmail.com>
2025-10-24 08:47:22 +0100

    Corrected the title alignment.

--- frontend/src/components/Title.tsx
+++ frontend/src/components/Title.tsx
@@ -3,7 +3,7 @@
 function Title({title, align}: {title: string; align?: string}) {
   return (
     <div>
-      <h1 className={`text-${align} text-[32px] uppercase font-bold`}>{title}</h1>
+      <h1 className={`text-center sm:text-${align} text-[32px] uppercase font-bold pb-3`}>{title}</h1>
     </div>
   )
 }
