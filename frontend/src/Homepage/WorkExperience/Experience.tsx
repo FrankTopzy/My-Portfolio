b0d2c2a70a637f964f3396e96c2bff0f8317eedc
Frank <temitopeadeoye787898@gmail.com>
2025-10-24 08:45:46 +0100

    Added the vertical-timeline component to the Experience section.

--- frontend/src/Homepage/WorkExperience/Experience.tsx
+++ frontend/src/Homepage/WorkExperience/Experience.tsx
@@ -1,9 +1,14 @@
+import Title from "../../components/Title"
+import VerticalTimelin from "../../components/VerticalTimelin"
 
 
 function Experience() {
   return (
-    <div className="bg-black text-white flex flex-col h-[70vh]" id="experience">
-      Work Experience
+    <div className="bg-black text-white flex flex-col items-center" id="experience">
+      <section className="pt-[100px] lg:px-[200px] px-10 pb-10 max-w-[1300px]">
+        <Title title="Work Experience" align="left"/>
+        <VerticalTimelin/>
+      </section>
     </div>
   )
 }
