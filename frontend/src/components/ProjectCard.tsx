ced29c7abcee35e91005325e981c6434189b435f
FrankTopzy <166184827+FrankTopzy@users.noreply.github.com>
2025-10-30 09:42:34 +0100

    Started designing the project card

---
+++ frontend/src/components/ProjectCard.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Tilt from 'react-parallax-tilt'
+
+function ProjectCard() {
+  return (
+    <Tilt>
+      <div>
+        <div>
+          <img src="" alt="hotelmang" />
+        </div>
+
+        <div>
+          info
+        </div>
+      </div>
+    </Tilt>
+   
+  )
+}
+
+export default ProjectCard
