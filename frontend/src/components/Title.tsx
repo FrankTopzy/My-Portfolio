9f1ce5dba65d00aaf351cbb3f3393375b486b816
FrankTopzy <166184827+FrankTopzy@users.noreply.github.com>
2025-10-30 09:53:15 +0100

    Added the pad parameter to manually add padding to every title.

--- frontend/src/components/Title.tsx
+++ frontend/src/components/Title.tsx
@@ -1,6 +1,6 @@
 //import React from 'react'
 
-function Title({title, align}: {title: string; align?: string}) {
+function Title({title, align, pad}: {title: string; align?: string; pad?: number}) {
   return (
     <div>
       <h1 className={`text-center sm:text-${align} text-[32px] uppercase font-bold pb-3`}>{title}</h1>
