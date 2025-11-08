c8ebd803f100432bdfbb1919b0b9361ca60c7a5b
FrankTopzy <166184827+FrankTopzy@users.noreply.github.com>
2025-11-08 20:28:31 +0100

    Started the footer design.

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,9 +1,14 @@
-
+import { FaCopyright } from "react-icons/fa6"
 
 function Footer() {
+  const date = new Date();
+  const year = date.getFullYear();
+
   return (
-    <div className="flex flex-col h-[40vh]">
-      Footer
+    <div className="flex flex-col text-[var(--navbar-bg)] py-4 min-h-[10vh] items-center font-bold">
+      <p>Built with TypeScript, ReactJs, Vite.js, TailwindCss, Framer Motion, and Vercel.</p>
+      <p>Star this project on Github</p>
+      <p className="flex items-center gap-2"><FaCopyright/> Frank Topzy {year}</p>
     </div>
   )
 }
