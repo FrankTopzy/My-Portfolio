b8d21052860aeba6531b99ed158d7ebbcb8991d4
FrankTopzy <166184827+FrankTopzy@users.noreply.github.com>
2025-11-08 18:47:28 +0100

    Interchanged the Logo color while scrolling.

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -41,7 +41,7 @@ function Navbar({isOpen, setIsOpen}: NavbarType) {
       <header className={`${Styles.header} flex items-center justify-between gap-[150px] fixed top-5 bg-[var(--navbar-bg)] text-[#E6E49F] py-1 xl:py-3 px-3 xl:px-10 rounded-xl hover:scale-105 transition-all z-100`}>
         <div className="flex items-center font-[Lobster] font-bold">
           <BiCode className='text-2xl font-bold'/>
-          <span className={`${scrollUp ? 'text-4xl text-[#E6E49F]' : 'text-5xl text-white'} max-md:text-3xl lg: xl: transition-all`}>TA</span>
+          <span className={`${scrollUp ? 'text-4xl text-white' : 'text-5xl text-[#E6E49F]'} max-md:text-3xl lg: xl: transition-all`}>TA</span>
           <BiCodeAlt className='text-2xl font-bold'/>
         </div>
 
