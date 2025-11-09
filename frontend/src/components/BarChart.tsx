53d78bb0dd217726afe2212ed3f5fe4e27ce970c
FrankTopzy <166184827+FrankTopzy@users.noreply.github.com>
2025-11-09 23:40:43 +0100

    Added the barchart file.

---
+++ frontend/src/components/BarChart.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import { BarChart } from '@mui/x-charts/BarChart';
+import { dataset, valueFormatter } from '../data/constants';
+
+const chartSetting = {
+  xAxis: [{ label: 'rainfall (mm)' }],
+  height: 400,
+  margin: { left: 0 },
+};
+
+export default function GridDemo() {
+  return (
+    <BarChart
+      dataset={dataset}
+      yAxis={[{ scaleType: 'band', dataKey: 'month' }]}
+      series={[{ dataKey: 'seoul', label: 'Seoul rainfall', valueFormatter }]}
+      layout="horizontal"
+      grid={{ vertical: true }}
+      {...chartSetting}
+    />
+  );
+}
