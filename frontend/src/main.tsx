748d336c19f0757caead74c7e6b83166e9b752a9
FrankTopzy <166184827+FrankTopzy@users.noreply.github.com>
2025-11-09 23:40:23 +0100

    Wrapped the App component with the ContextProvider.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from "react-router-dom"
 import './index.css'
 import App from './App.tsx'
+import PortfolioContextProvider from './components/Context.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <PortfolioContextProvider>
+        <App />
+      </PortfolioContextProvider>
     </BrowserRouter>
   </StrictMode>,
 )
