3897e65c58ea55bea1d43c32882c82379053774b
Frank <temitopeadeoye787898@gmail.com>
2025-11-15 01:40:07 +0100

    Completed the contact me section.

--- src/Homepage/Contact/ContactMe.tsx
+++ src/Homepage/Contact/ContactMe.tsx
@@ -1,10 +1,13 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Title from '../../components/Title'
 // npm install react-hook-form @web3forms/react
 import { useForm } from "react-hook-form";
 import useWeb3Forms from "@web3forms/react";
+import { FaCheck } from 'react-icons/fa6';
+import { FaTimes } from 'react-icons/fa';
 
 function ContactMe() {
+  const [popup, setPopup] = useState(false);
 
   /*useEffect(() => {
     console.log(message);
@@ -24,17 +27,28 @@ function ContactMe() {
       subject: "New Contact Message from your Website",
       // ... other settings
     },
-    onSuccess: (msg, data) => {
+    onSuccess: (msg) => {
       setIsSuccess(true);
+      setPopup(true);
       setResult(msg);
-      reset(data);
+      reset();
     },
-    onError: (msg, data) => {
+    onError: (msg) => {
       setIsSuccess(false);
+      setPopup(true);
       setResult(msg);
-      reset(data);
+      reset();
     },
   });
+
+  useEffect(() => {
+    let timer;
+    if (isSuccess) {
+      timer = setTimeout(() => {
+        setPopup(false);
+      }, 3000); // Popup will disappear after 3 seconds
+    }
+  }, [isSuccess]);
 
   return (
     <div className="bg-black text-white flex min-h-[70vh] pt-[100px] pb-[40px] justify-center" id="contact">
@@ -45,23 +59,28 @@ function ContactMe() {
           <input type="hidden" name="access_key" value="74fbda5c-ec04-4c4c-954a-029a2d682f90"></input>
           <div className='flex flex-col gap-3'>
             <div className='flex flex-col gap-3 md:flex-row md:gap-0 justify-between'>
-              <input type="text" {...register("first_name", { required: true })} placeholder='Enter Your First Name...' className='md:w-[49%] w-full px-3 py-1.5 bg-amber-300' required/>
-              <input type="text" {...register("last_name", { required: true })} placeholder='Enter Your Last Name...' className='px-3 py-1.5 md:w-[49%] w-full bg-green-600' required/>
+              <input type="text" {...register("first_name", { required: true })} placeholder='Enter Your First Name...' className='md:w-[49%] w-full px-3 py-2.5 bg-amber-300' required/>
+              <input type="text" {...register("last_name", { required: true })} placeholder='Enter Your Last Name...' className='px-3 py-2.5 md:w-[49%] w-full bg-green-600' required/>
             </div>
 
             <div className='flex flex-col gap-3 md:flex-row md:gap-0 justify-between'>
-              <input type="text" {...register("number", { required: true })} placeholder='Enter Your Mobile/Whatsapp Number....' className='md:w-[49%] w-full px-3 py-1.5 bg-amber-200' required/>
-              <input type="email" {...register("email", { required: true })} placeholder='Enter Your Email Address...' className='px-3 py-1.5 md:w-[49%] w-full bg-amber-950' required/>
+              <input type="text" {...register("number", { required: true })} placeholder='Enter Your Mobile/Whatsapp Number....' className='md:w-[49%] w-full px-3 py-2.5 bg-amber-200' required/>
+              <input type="email" {...register("email", { required: true })} placeholder='Enter Your Email Address...' className='px-3 py-2.5 md:w-[49%] w-full bg-amber-950' required/>
             </div>
           </div>
 
           <div className=''>
-            <textarea {...register("message", { required: true })} id="" placeholder='Your Message...' className='px-3 py-1.5 h-[300px] w-full bg-amber-500'></textarea>
+            <textarea {...register("text", { required: true })} id="" placeholder='Your Message...' className='px-3 pt-2.5 h-[300px] w-full bg-amber-950'></textarea>
           </div>
 
-          <button className='bg-[var(--navbar-bg)] self-center px-7 py-2 rounded-xl'>Send Message</button>
+          <button className='bg-[var(--navbar-bg)] self-center px-7 py-2 rounded-xl hover:bg-[var(--color-2)] hover:text-black transition-all'>Send Message</button>
 
-          {isSuccess && (<div>{result}</div>)}
+          {isSuccess && (<div className={`popup w-[90%] sm:w-[45%] md:w-[35%] relative ${popup ? 'show' : ''}`}>
+            <p>Email {!isSuccess && 'not'} sent successfully!</p>
+            <p className=' rounded-full bg-[var(--navbar-bg)] p-2.5'><FaCheck className='text-green-500 text-2xl'/></p>
+
+            <FaTimes className='absolute top-2 right-2' onClick={() => setPopup(false)}/>
+          </div>)}
         </form>
       </section>
     </div>
